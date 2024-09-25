6bb85a03b46336b86bc37827257eab7b42f17b94
Haruya Nakamura <hareruya.maro@gmail.com>
2024-09-25 23:12:03 +0900

    add demo expo app

---
+++ demo/app/(tabs)/_layout.tsx
@@ -0,0 +1,44 @@
+import { Tabs } from "expo-router";
+import React from "react";
+
+import { TabBarIcon } from "@/components/navigation/TabBarIcon";
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Section",
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon
+              name={focused ? "list" : "list-outline"}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="grid"
+        options={{
+          title: "Grid",
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon
+              name={focused ? "grid" : "grid-outline"}
+              color={color}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ demo/app/(tabs)/grid.tsx
@@ -0,0 +1,86 @@
+import { SafeAreaView, StatusBar, StyleSheet, Text, View } from "react-native";
+import { FlashSectionList } from "react-native-flash-section-list";
+
+const DATA = [
+  {
+    title: "Main dishes",
+    data: ["Pizza", "Burger", "Risotto"],
+  },
+  {
+    title: "Sides",
+    data: ["French Fries", "Onion Rings", "Fried Shrimps"],
+  },
+  {
+    title: "Drinks",
+    data: ["Water", "Coke", "Beer"],
+  },
+  {
+    title: "Desserts",
+    data: ["Cheese Cake", "Ice Cream"],
+  },
+  {
+    title: "Main dishes",
+    data: ["Pizza", "Burger", "Risotto"],
+  },
+  {
+    title: "Sides",
+    data: ["French Fries", "Onion Rings", "Fried Shrimps"],
+  },
+  {
+    title: "Drinks",
+    data: ["Water", "Coke", "Beer"],
+  },
+  {
+    title: "Desserts",
+    data: ["Cheese Cake", "Ice Cream"],
+  },
+];
+
+export default function GridScreen() {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <FlashSectionList
+        sections={DATA}
+        keyExtractor={(item, index) => {
+          if (item.type === "sectionHeader") {
+            return item.section.title + index;
+          } else {
+            return item.item + index;
+          }
+        }}
+        renderItem={({ item }: { item: string }) => (
+          <View style={styles.item}>
+            <Text style={styles.title}>{item}</Text>
+          </View>
+        )}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text style={styles.header}>{title}</Text>
+        )}
+        numColumns={2}
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+    marginHorizontal: 16,
+  },
+  item: {
+    backgroundColor: "#f9c2ff",
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    width: "80%",
+  },
+  header: {
+    fontSize: 32,
+    backgroundColor: "#fff",
+    width: "100%",
+  },
+  title: {
+    fontSize: 24,
+  },
+});
